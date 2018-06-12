9ddfae093aaa9da8345a56e41d32f700fb41b540
RareScripting <40193760+RareScripting@users.noreply.github.com>
2018-06-12 17:40:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const prefix = "!";
-bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.tag}!`);
-});
+
 
 bot.on("message", (message)=>{
 if(!message.content.startsWith(prefix)) return;
